9f0e223df791c30c64236d2da529a9fa78452d92
Roman Khomyn <107980144+khrom22@users.noreply.github.com>
2023-11-06 23:03:19 +0200

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,1 @@
-const navElement = document.querySelector('#categories');
-const listItem = navElement.querySelectorAll('.item');
-console.log(`Number of categories: ${listItem.length}`);
-console.log("");
-for (let i = 0; i < listItem.length; i++) {
-    console.log("Category: " + listItem[i].querySelector('h2').textContent);
-    console.log("Elements: " + listItem[i].querySelectorAll('ul li').length);
-    console.log("");
-}
+const list = document.querySelector('#categories')
